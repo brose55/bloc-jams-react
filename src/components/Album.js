ec185017db75685025efdc89fa408707e6b98949
B Rose <brandonrose805@gmail.com>
2018-03-08 23:03:03 -0800

    Assignment 8 Player Bar: Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,14 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(currentIndex + 1, 4);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return (
       <section className="album">
@@ -94,6 +102,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
